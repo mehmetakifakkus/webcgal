8b8e25cf5f4075f250e2dadf6a1a2b5222a6de18
mehmetakifakkus <mehmetakifakkus@gmail.com>
2016-09-25 00:41:35 +0300

    documentation page is updated. Live Demo part is added

--- app/routes/documentationRoute.js
+++ app/routes/documentationRoute.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var ejs = require('ejs');
+var fs   = require('fs');
+
 
 router.get('/documentation', function(req, res) {
 
-    var selfLocal = req.app.locals;
+    req.app.locals.someHelper = function(name){
+        return ("hello " + name);
+    }
+
+  res.render('documentation', {
+    pageTitle: 'Documentation',
+    pageID: 'documentation'
+  });
+});
+
+router.get('/documentation/:item', function(req, res) {
+  var text, code;
+  var data = req.app.get('documentsData');
+
+      var selfLocal = req.app.locals;
 
     req.app.locals.findandChange = function(str){
         var output = str, re_start = new RegExp('link_to\\(','gi'), re_end = new RegExp('link_to\\(([a-z]+\\/*)+\\)','gi'); // 'gi' is  global match and ignore case
@@ -20,37 +37,40 @@ router.get('/documentation', function(req, res) {
     req.app.locals.myConcat = function(list){
         var total = '';
         var sl = selfLocal;
+
         list.forEach(function(item){ 
             var res = sl.findandChange(item); 
             while(res != item){item = res; res = sl.findandChange(item);} 
-            total += item;});
+            total += item;
+        });
+
+        //str = "<%d = 3%><h1>merhaba <%= d %></h1><%- include('filename', {filename:'../views/partials/content/documentationPaperStart'}); %>"
+        //str = "<%d = 3%><h1>merhaba <%= d %></h1>"
+        //var html = ejs.render(str);
+
+        //var fn = ejs.compile(fs.readFileSync('../views/partials/content/documentationPaperStart', 'utf-8'), { basedir: __dirname});
+
+        //console.log(fn());
+
         return total;
     } 
     
     req.app.locals.someHelper = function(name){
         return ("hello " + name);
     }
-    
-  res.render('documentation', {
-    pageTitle: 'Documentation',
-    pageID: 'documentation',
-    pageURL: req.originalUrl  
-  });
-});
 
-router.get('/documentation/:item', function(req, res) {
-  var text;   
-  var data = req.app.get('documentsData');  
     
   data.objects.forEach(function(it){ // search in objects
     if(it.name == req.params.item){
-         text = it.text;   
+         text = it.text;
+         code = it.code;
     }  
   });    
     
   data.functions.forEach(function(it){ // search in functions
     if(it.name == req.params.item){
          text = it.text;
+         code = it.code;
     }
   });
 
@@ -58,7 +78,7 @@ router.get('/documentation/:item', function(req, res) {
     pageTitle: req.params.item,
     pageID: req.params.item,
     pageText: text,
-    pageURL: req.originalUrl  
+    pageCode: code
   });
 });
 
--- app/views/partials/content/documentationPaperEnd.js
+++
@@ -1,5 +0,0 @@
-</script>
-    <div class="canvas down">
-        <canvas width="600px" height="320px" id="canvas-<%= canvas_id %>"></canvas>
-    </div>
-</div>

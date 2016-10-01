c3eafd8a1e28e12986b2e15e278b586ecabb936f
mehmetakifakkus <mehmetakifakkus@gmail.com>
2016-10-01 17:48:36 +0300

    put files again

---
+++ app/views/partials/paperjs/Polygon.js
@@ -0,0 +1,56 @@
+function Polygon(name){
+
+
+    //if ((typeof Polygon.id == 'undefined'))
+      //  Polygon.id = 0;
+
+    this.name = name;
+    this.points = new Object();
+    this.path = null;
+    var tempObj = this;
+
+    this.addPoint = function(p){
+        var polPoint = new PolygonPoint(p);
+        tempObj.points['p' + Object.keys(tempObj.points).length] = polPoint;
+    }
+
+    this.changePosition = function(delta){
+        for (var i in tempObj.points)
+            tempObj.points[i].pos += delta;
+        tempObj.draw();
+    }
+
+    this.changePosition2 = function(polPoint, delta){
+        tempObj.points[polPoint.name].pos += delta;
+        tempObj.draw();
+    }
+
+    this.draw = function(){ // silip tekrar cizdirir
+
+        clearDrawables();
+
+        for (var i in tempObj.points)           //for(var i=0; i<tempObj.points.length; i++) // this is array version
+            tempObj.points[i].draw(i);
+
+        tempObj.path = new Path({name: 'path_polygon_'+this.name, fillColor: 'white', strokeColor: 'black', strokeWidth: 2});
+        tempObj.path.sendToBack();
+        tempObj.path.closed = true;
+
+        for (var i in tempObj.points)
+            tempObj.path.add(tempObj.points[i].pos);
+
+        drawables.push(tempObj.path);
+
+        tempObj.path.onMouseDrag = function(event){
+            tempObj.mouseDragEvent(event);
+        }
+    };
+
+    this.mouseDragEvent = function(){
+        console.log('mouse drag event on a polygon')
+    }
+
+    this.sort = function(){
+        tempObj.points.sort(function(a, b) {return a.point.x - b.point.x;});
+    }
+};

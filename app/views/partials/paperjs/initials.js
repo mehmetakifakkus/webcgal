110f0b53554c3d7f3405058018e5ebf8f5cf488d
mehmetakifakkus <mehmetakifakkus@gmail.com>
2016-10-01 13:51:50 +0300

    convexHull code is added.

--- app/views/partials/paperjs/initials.js
+++ app/views/partials/paperjs/initials.js
@@ -3,3 +3,7 @@ project.activeLayer.transform( new Matrix(1,0,0,-1, 0, view.size.height) );
 
 var origin = new Point(0, 0);
 var drawables = [];
+
+var colors = ['red', 'blue', 'yellow', 'magenta', 'green'];
+var path = null, polPoint = null, textPoint = null;
+polygon = null;
--- app/views/partials/paperjs/polygonPoint.js
+++ app/views/partials/paperjs/polygonPoint.js
@@ -26,3 +26,56 @@ function PolygonPoint(p){
     
     return this;
 };
+
+
+function TextPoint(x,y){
+
+    if ((typeof TextPoint.id == 'undefined'))
+        TextPoint.id = 0;
+
+    this.id = TextPoint.id++;
+
+    this.pos = new Point(x,y);
+    this.path = null;
+
+    var self = this;
+
+    this.draw = function(){
+
+        drawables.push(new PointText({ matrix: new Matrix(1,0,0,-1, self.pos.x+3, self.pos.y+5), fontSize: '16px', fillColor: 'black', content: ' '+ self.id}));
+
+        self.path = new Path.Circle({name: '_p'+self.id, center: self.pos, radius: 5, fillColor: 'black'});
+        drawables.push(self.path);
+
+        self.path.onMouseDrag = function(event){
+            self.mouseDragEvent(event);
+        }
+    };
+    this.draw();
+
+
+    this.mouseDragEvent = function(event){
+
+        clearDrawables();
+
+        console.log('default mouse drag event on a TextPoint '+self.id)
+        self.pos += event.delta * [1, -1];
+
+        for(var i=0; i < points.length; i++)
+            points[i].draw();
+
+        convexHull(points);
+    };
+
+    this.toString = function() {
+        return ''+self.path.name+' ';
+        //        return ''+self.path.name+' '+self.pos;
+
+    };
+
+    this.valueOf = function(){
+        return self.pos;
+    }
+
+    //return this.pos;
+};
